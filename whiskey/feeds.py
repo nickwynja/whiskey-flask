ed20a3f28b03012d39afc151734f53174d7b2b56
Nick Wynja <nick@nickwynja.com>
2024-07-16 13:50:57 -0400

    cache log feed

--- whiskey/feeds.py
+++ whiskey/feeds.py
@@ -4,7 +4,8 @@ import glob
 import datetime
 import os
 import pypandoc
-from flask import url_for, Response, redirect
+import feedparser
+from flask import url_for, Response, redirect, send_from_directory
 from pytz import timezone
 from feedgen.feed import FeedGenerator
 from pathlib import Path
@@ -59,29 +60,65 @@ def feed():
 
 @app.route('/log.xml')
 def log():
-    feed = FeedGenerator()
-    feed.title(f"{app.config.get('AUTHOR')}'s Log")
-    feed.link(href=app.config['BASE_URL'] + url_for('log'), rel='self')
-    feed.subtitle(app.config.get('DESCRIPTION', ""))
-    feed.author(name=app.config.get('AUTHOR', ""))
-    feed.id(feed.title())
-    feed.link(href=app.config['BASE_URL'], rel='alternate')
-
     files = sorted(glob.glob("./content/data/log/*"))
+    cache_name = "log.xml"
+
+    p = []
 
     for file in files:
         with open(file) as f:
             d = Path(f.name).stem
             t = datetime.datetime.strptime(d, '%Y%m%d%H%M%S%z')
             l = f.read()
+            i = {
+                    "filename": f.name,
+                    "date": d,
+                    "timestamp": t,
+                    "content": l,
+                }
+            p.append(i)
 
-        entry = feed.add_entry()
-        entry.id(d.split("-")[0])
-        entry.published(t)
-        entry.author(name=app.config.get('AUTHOR', ""))
-        if Path(f.name).suffix == ".md":
-            entry.content(pypandoc.convert_text(l, 'html', format='md'), type="html")
-        else:
-            entry.content(l, type="html")
+    try:
+        latest_post = p[-1]['timestamp']
+    except (KeyError, IndexError) as e:
+        latest_post = None
 
-    return Response(feed.rss_str(pretty=True), mimetype="application/rss+xml")
+    try:
+        cached_xml = feedparser.parse(f"{app.config['STATIC_FOLDER']}/{cache_name}")
+        cached_time = datetime.datetime.strptime(cached_xml['feed']['updated'], "%a, %d %b %Y %H:%M:%S %z")
+    except KeyError as e:
+        cached_time = None
+
+    if (
+            cached_time is None
+            or latest_post is None
+            or latest_post > cached_time):
+
+        ## update feed
+        feed = FeedGenerator()
+        feed.title(f"{app.config.get('AUTHOR')}'s Log")
+        feed.link(href=app.config['BASE_URL'] + url_for('log'), rel='self')
+        feed.subtitle(app.config.get('DESCRIPTION', ""))
+        feed.author(name=app.config.get('AUTHOR', ""))
+        feed.id(feed.title())
+        feed.link(href=app.config['BASE_URL'], rel='alternate')
+        feed.lastBuildDate(latest_post)
+
+
+        for i in p:
+            entry = feed.add_entry()
+            entry.id(i['date'].split("-")[0])
+            entry.published(i['timestamp'])
+            entry.author(name=app.config.get('AUTHOR', ""))
+            if Path(i['filename']).suffix == ".md":
+                entry.content(pypandoc.convert_text(i['content'], 'html', format='md'), type="html")
+            else:
+                entry.content(i['content'], type="html")
+
+        feed.rss_file(f"{app.config['STATIC_FOLDER']}/{cache_name}", pretty=True)
+
+    return send_from_directory(
+        app.config['STATIC_FOLDER'],
+        cache_name,
+        mimetype="application/rss+xml"
+    )
